95183ee75c7e174011ebe66d11114dc315587a8c
eregon <eregontp@gmail.com>
2010-06-15 13:22:03 +0200

    057 & replace tabs by spaces

--- 053.rb
+++ 053.rb
@@ -3,17 +3,14 @@ There are exactly ten ways of selecting three from five, 12345:
 
 123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
 
-In combinatorics, we use the notation, ^(5)C_(3) = 10.
+In combinatorics, we use the notation, C_(3,5) = 10.
 
 In general,
-^(n)C_(r) =
-n!
-r!(n−r)!
-	,where r ≤ n, n! = n×(n−1)×...×3×2×1, and 0! = 1.
+C_(r,n) = n! / r!(n−r)!, where r ≤ n, n! = n×(n−1)×...×3×2×1, and 0! = 1.
 
-It is not until n = 23, that a value exceeds one-million: ^(23)C_(10) = 1144066.
+It is not until n = 23, that a value exceeds one-million: C_(10,23) = 1144066.
 
-How many, not necessarily distinct, values of  ^(n)C_(r), for 1 ≤ n ≤ 100, are greater than one-million?
+How many, not necessarily distinct, values of C_(r,n), for 1 ≤ n ≤ 100, are greater than one-million?
 =end
 
 require_relative 'lib/integer/factorial'
---
+++ 057.rb
@@ -0,0 +1,34 @@
+=begin
+It is possible to show that the square root of two can be expressed as an infinite continued fraction.
+
+√ 2 = 1 + 1/(2 + 1/(2 + 1/(2 + ... ))) = 1.414213...
+
+By expanding this for the first four iterations, we get:
+
+1 + 1/2 = 3/2 = 1.5
+1 + 1/(2 + 1/2) = 7/5 = 1.4
+1 + 1/(2 + 1/(2 + 1/2)) = 17/12 = 1.41666...
+1 + 1/(2 + 1/(2 + 1/(2 + 1/2))) = 41/29 = 1.41379...
+
+The next three expansions are 99/70, 239/169, and 577/408, but the eighth expansion, 1393/985, is the first example where the number of digits in the numerator exceeds the number of digits in the denominator.
+
+In the first one-thousand expansions, how many fractions contain a numerator with more digits than denominator?
+=end
+
+require_relative 'lib'
+
+# numbers = [2,3,4]
+# 2 + 1/( 3+1/( 4 ) )
+# 2 + Rational(1,3 + Rational(1,4) )
+def continued_fraction(numbers)
+  last = numbers.pop
+  numbers.reverse_each.inject(last) { |r, n|
+    n + Rational(1, r)
+  }
+end
+
+p (1..1_000).count { |expansion|
+  r = continued_fraction([1]+[2]*expansion)
+  r.numerator.ndigits > r.denominator.ndigits
+} # => 153, 4s
+
