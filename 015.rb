4b069e979e2b56e1d0369906989f39d944bd00c2
eregon <eregontp@gmail.com>
2010-06-18 19:25:14 +0200

    adding #15

--- 015.rb
+++ 015.rb
@@ -1,10 +1,20 @@
-# Starting in the top left corner of a 2×2 grid,
-#  there are 6 routes (without backtracking) to the bottom right corner.
-# How many routes are there through a 20×20 grid?
-SIZE = 2#0
-routes_length = SIZE*2
-require_relative '../_class/math/point'
+=begin
+Starting in the top left corner of a 2×2 grid,
+ there are 6 routes (without backtracking) to the bottom right corner.
+How many routes are there through a 20×20 grid?
+=end
 
-start = Point.new(0,0)
+require_relative 'lib'
 
+def test way
+  puts way
+  ((1..5).to_a << 20).each { |i| puts "#{i}*#{i}: #{yield i}" }
+  puts
+end
 
+test(:theoric_combinations) { |n| (2*n).! / n.!**2 }
+
+test(:generating_combinations) { |n|
+  next "stopped because it takes forever ;)" if n == 20
+  [:down, :right].repeated_permutation(2*n).select { |perm| perm.count(:down) == perm.size/2 }.size
+}
