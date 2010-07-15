c9c0180ab66fa9a0367d8324fde6779d099d9bc1
eregon <eregontp@gmail.com>
2010-07-15 15:29:40 +0200

    Improve style of #1

--- 001.rb
+++ 001.rb
@@ -1,7 +1,9 @@
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
-# The sum of these multiples is 23.
-# Find the sum of all the multiples of 3 or 5 below 1000.
+=begin
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.
+=end
 
-p 1.upto(999).inject(0) { |sum, i|
+p (1...1000).inject(0) { |sum, i|
   (i % 3 == 0 or i % 5 == 0) ? sum + i : sum
 } # => 233168
