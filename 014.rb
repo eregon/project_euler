d4bf6567347c2e3d5417f04eb8c0bd70d2f5f6f2
eregon <eregontp@gmail.com>
2010-08-23 19:35:43 +0200

    add some crazy memoize stuff

--- 014.rb
+++ 014.rb
@@ -36,6 +36,8 @@
 
 
 ###### Using monkey-patching
+require "benchmark"
+
 =begin
 class Integer
   attr_accessor :calc
@@ -56,9 +58,89 @@ class Integer
   end
 end
 
-p (1..1_000_000).max_by(&:sequence) # 837799, 7.3s
+#p (1..1_000_000).max_by(&:sequence) # 837799, 7.3s
 
-# p 13.sequence
-# p 40.sequence
-# p 13.sequence
-# p 40.sequence
+# With Memoize
+class Method
+  def memoize
+    name = name()
+    org = "org_#{name}"
+    cache = {}
+    Object.class_eval {
+      alias_method(org, name)
+      define_method(name) { |arg|
+        if cache.key? arg
+          cache[arg]
+        else
+          cache[arg] = send(org, arg)
+        end
+      }
+    }
+    self
+  end
+
+  def memoize2
+    cache = {}
+    name = name()
+    lambda { |*args|
+      if cache.key? args
+        cache[args]
+      else
+        cache[args] = args.shift.send(name, *args)
+      end
+    }
+  end
+end
+
+class Integer
+  def seq(n = 1)
+    self == 1 ? n : (self.even? ? self/2 : 3*self+1).seq(n+1)
+  end
+end
+
+def seqr(n)
+  n == 1 ? 1 : 1 + seqr(n.even? ? n/2 : 3*n+1)
+end
+
+def seqf(n)
+  n == 1 ? 1 : 1 + seqf(n.even? ? n/2 : 3*n+1)
+end
+
+seqm = 2.method(:seq).memoize2
+#seqf = method(:seqf).memoize
+
+method(:seqf).memoize
+
+p seqf(13)
+p seqf(40)
+p seqf(50)
+#exit
+(1..100).each { |i|
+  a, b, c, d = i.sequence, seqf(i), seqm[i], seqf(i)
+  puts i unless a == b and b == c and c == d
+}
+
+N = (1..25_000)
+Benchmark.bm(20) do |x|
+  x.report("recursive (no cached)") {
+    N.each { |i| seqr(i) }
+  }
+  x.report("memoize") {
+    N.each { |i| seqf(i) }
+  }
+  x.report("memoize2") {
+    seq_m = 0.method(:seq).memoize2
+    N.each { |i| seq_m[i] }
+  }
+  x.report("internal cache") {
+    N.each { |i| i.sequence }
+  }
+end
+
+__END__
+                          user     system      total      real
+recursive (no cached)  0.430000   0.000000   0.430000 (0.429842)
+memoize                0.070000   0.010000   0.080000 (0.085660)
+memoize2               0.580000   0.000000   0.580000 (0.589766)
+internal cache         0.090000   0.000000   0.090000 (0.093896)
+
